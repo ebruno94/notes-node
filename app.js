4e423cd805b7f8ab753c7fb206949a4fa88226d4
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-08-13 14:53:22 -0700

    Add functionality to list and take adavantage of display note

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js...');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,8 +6,6 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs', argv);
 
 if (command === 'add'){
   var note = notes.addNote(argv.title, argv.body);
@@ -21,8 +17,11 @@ if (command === 'add'){
   }
 
 } else if (command === 'list'){
-  console.log('Listing ...');
-  console.log(notes.getAll());
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s)...`);
+  allNotes.forEach((note) => {
+    notes.displayNote(note);
+  });
 
 } else if (command === 'read'){
   var note = notes.getNote(argv.title);
